4dc1c53d54f18ce90cbc228c5afa48e435e875ad
linuxing3 <linuxing3@qq.com>
2021-11-01 12:37:28 -0400

    Regular commit on 2021-11-01 周一 12:37:28.63

--- init.el
+++ init.el
@@ -1,37 +1,37 @@
-;; ---------------------------------------------------------
+﻿;; ---------------------------------------------------------
 ;; 包管理器
 ;; ---------------------------------------------------------
-(load-file "~/OneDrive/config/emacs/scratch/modules/packages.el")
+(load-file "~/EnvSetup/config/evil-emacs/modules/packages.el")
 
 ;; ---------------------------------------------------------
 ;; 自动加载帮助器
 ;; ---------------------------------------------------------
-(load-file "~/OneDrive/config/emacs/scratch/modules/autoloads.el")
+(load-file "~/EnvSetup/config/evil-emacs/modules/autoloads.el")
 
 ;; ---------------------------------------------------------
 ;; 功能模块
 ;; ---------------------------------------------------------
 
-(load-file (dropbox-path "config/emacs/scratch/modules/base.el"))
-(load-file (dropbox-path "config/emacs/scratch/modules/ivy.el"))
-(load-file (dropbox-path "config/emacs/scratch/modules/ui.el"))
-(load-file (dropbox-path "config/emacs/scratch/modules/evil-mode.el"))
+(load-file "~/EnvSetup/config/evil-emacs/modules/base.el")
+(load-file "~/EnvSetup/config/evil-emacs/modules/ivy.el")
+(load-file "~/EnvSetup/config/evil-emacs/modules/ui.el")
+(load-file "~/EnvSetup/config/evil-emacs/modules/evil-mode.el")
 
 ;; ---------------------------------------------------------
 ;; Org功能模块
 ;; ---------------------------------------------------------
 
-(load-file (dropbox-path "config/emacs/scratch/modules/org-mode.el"))
+(load-file "~/EnvSetup/config/evil-emacs/modules/org-mode.el")
 
 ;; ---------------------------------------------------------
 ;; 编程模块
 ;; ---------------------------------------------------------
-(load-file (dropbox-path "config/emacs/scratch/modules/project.el"))
-(load-file (dropbox-path "config/emacs/scratch/modules/completion.el"))
-(load-file (dropbox-path "config/emacs/scratch/modules/format.el"))
-(load-file (dropbox-path "config/emacs/scratch/modules/coding-mode.el"))
+(load-file "~/EnvSetup/config/evil-emacs/modules/project.el")
+(load-file "~/EnvSetup/config/evil-emacs/modules/completion.el")
+(load-file "~/EnvSetup/config/evil-emacs/modules/format.el")
+(load-file "~/EnvSetup/config/evil-emacs/modules/coding-mode.el")
 
 ;; ---------------------------------------------------------
 ;; 快捷键绑定
 ;; ---------------------------------------------------------
-(load-file (dropbox-path "config/emacs/scratch/modules/key-bindings.el"))
+(load-file "~/EnvSetup/config/evil-emacs/modules/key-bindings.el")
