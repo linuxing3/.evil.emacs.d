f0b96cd5741212e3764bef3846b7a7d0feaa3554
linuxing3 <linuxing3@qq.com>
2021-11-04 15:00:01 -0400

    open with extrnal app fun

--- modules/autoloads.el
+++ modules/autoloads.el
@@ -66,6 +66,42 @@
     (setq url-proxy-services nil)
     (message "代理已关闭.")))
 
+;; 使用外部应用打开
+(defun open-with-external-app (&optional @fname)
+  "Open the current file or dired marked files in external app.
+When called in Emacs Lisp, if @FNAME is given, open that."
+  (interactive)
+  (let* (
+         ($file-list
+          (if @fname
+              (progn (list @fname))
+            (if (string-equal major-mode "dired-mode")
+                (dired-get-marked-files)
+              (list (buffer-file-name)))))
+         ($do-it-p (if (<= (length $file-list) 5)
+                       t
+                     (y-or-n-p "Open more than 5 files? "))))
+    (when $do-it-p
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc
+         (lambda ($fpath)
+           (shell-command
+            (concat "PowerShell -Command \"Invoke-Item -LiteralPath\" " "'"
+                    (shell-quote-argument (expand-file-name $fpath )) "'")))
+         $file-list))
+       ((string-equal system-type "darwin")
+        (mapc
+         (lambda ($fpath)
+           (shell-command
+            (concat "open " (shell-quote-argument $fpath))))
+         $file-list))
+       ((string-equal system-type "gnu/linux")
+        (mapc
+         (lambda ($fpath)
+           (let ((process-connection-type nil))
+             (start-process "" nil "xdg-open" $fpath)))
+         $file-list))))))
 ;; ===============================================
 ;; 核心设置
 ;; ===============================================
