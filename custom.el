73440b2cc6c2915a1dad3147e325cfe95fbb327c
linuxing3 <linuxing3@qq.com>
2021-11-06 15:26:38 -0400

    key

--- custom.el
+++ custom.el
@@ -3,17 +3,50 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cua-mode t nil (cua-base))
+ '(ansi-color-names-vector
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#61bfff" "#ff79c6" "#8be9fd" "#f8f8f2"])
+ '(custom-safe-themes
+   '("93ed23c504b202cf96ee591138b0012c295338f38046a1f3c14522d4a64d7308" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default))
+ '(fci-rule-color "#6272a4")
  '(global-display-line-numbers-mode t)
+ '(jdee-db-active-breakpoint-face-colors (cons "#1E2029" "#bd93f9"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1E2029" "#50fa7b"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1E2029" "#565761"))
+ '(objed-cursor-color "#ff5555")
  '(org-agenda-files
-   '("c:/Users/wjb/OneDrive/org/xingwenju.agenda.org" "c:/Users/wjb/OneDrive/org/works.agenda.org" "c:/Users/wjb/OneDrive/org/projects.agenda.org" "c:/Users/wjb/OneDrive/org/lulu.agenda.org" "c:/Users/wjb/OneDrive/org/inbox.agenda.org" "c:/Users/wjb/OneDrive/org/daniel.agenda.org" "c:/Users/wjb/OneDrive/org/.#daniel.agenda.org"))
+   '("~/OneDrive/org/habit.agenda.org" "c:/Users/wjb/OneDrive/org/xingwenju.agenda.org" "c:/Users/wjb/OneDrive/org/works.agenda.org" "c:/Users/wjb/OneDrive/org/projects.agenda.org" "c:/Users/wjb/OneDrive/org/lulu.agenda.org" "c:/Users/wjb/OneDrive/org/inbox.agenda.org" "c:/Users/wjb/OneDrive/org/daniel.agenda.org"))
  '(org-support-shift-select t)
  '(package-selected-packages
-   '(dired+ org-agenda page-break-lines nyan-mode highlight-indent-guides info-colors all-the-icons-dired evil-leader lsp-mode yasnippet lsp-treemacs lsp-ui lsp-ivy projectile hydra flycheck company avy which-key dap-mode))
+   '(evil-multiedit dired+ org-agenda page-break-lines nyan-mode highlight-indent-guides info-colors all-the-icons-dired evil-leader lsp-mode yasnippet lsp-treemacs lsp-ui lsp-ivy projectile hydra flycheck company avy which-key dap-mode))
+ '(pdf-view-midnight-colors (cons "#f8f8f2" "#282a36"))
+ '(rustic-ansi-faces
+   ["#282a36" "#ff5555" "#50fa7b" "#f1fa8c" "#61bfff" "#ff79c6" "#8be9fd" "#f8f8f2"])
  '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode)))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#282a36")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50fa7b")
+    (cons 40 "#85fa80")
+    (cons 60 "#bbf986")
+    (cons 80 "#f1fa8c")
+    (cons 100 "#f5e381")
+    (cons 120 "#face76")
+    (cons 140 "#ffb86c")
+    (cons 160 "#ffa38a")
+    (cons 180 "#ff8ea8")
+    (cons 200 "#ff79c6")
+    (cons 220 "#ff6da0")
+    (cons 240 "#ff617a")
+    (cons 260 "#ff5555")
+    (cons 280 "#d45558")
+    (cons 300 "#aa565a")
+    (cons 320 "#80565d")
+    (cons 340 "#6272a4")
+    (cons 360 "#6272a4")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
