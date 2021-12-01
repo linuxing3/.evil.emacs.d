886c2d85353f9b0c0eb7aa71ed83890a2f94f143
linuxing3 <linuxing3@qq.com>
2021-11-30 21:35:15 -0500

    fix(pyim): Setting pyim intelisense

--- modules/module-input.el
+++ modules/module-input.el
@@ -1,3 +1,7 @@
+;; Chinese input method powered by `pyim'
+;; https://tmuashu.github.io/pyim
+;; 
+
 (use-package pyim
   :config
   (use-package pyim-basedict
@@ -8,17 +12,24 @@
   ;;   :ensure nil
   ;;   :config (pyim-wbdict-gbk-enable))
   (setq default-input-method "pyim")
+  (global-set-key (kbd "C-\\") 'toggle-input-method)
 
   ;; 我使用全拼
   (setq pyim-default-scheme 'quanpin)
+
+
+  ;; FIXME
+  ;; 只有前面有中文，才可以输入中文。
+  ;; 如果没有中文，该如何切换到中文输入呢？
 
   ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
   ;; 我自己使用的中英文动态切换规则是：
   ;; 1. 光标只有在注释里面时，才可以输入中文。
   ;; 2. 光标前是汉字字符时，才能输入中文。
-  ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+  ;; 3. 使用 C-; 快捷键，强制将光标前的拼音字符串转换为中文。
   (setq-default pyim-english-input-switch-functions
-                '(pyim-probe-dynamic-english
+                '(
+                  pyim-probe-dynamic-english
                   pyim-probe-isearch-mode
                   pyim-probe-program-mode
                   pyim-probe-org-structure-template))
@@ -27,6 +38,13 @@
                 '(pyim-probe-punctuation-line-beginning
                   pyim-probe-punctuation-after-punctuation))
 
+
+  ;; 转化为中文
+  (global-set-key (kbd "M-i") 'pyim-convert-code-at-point)
+  ;; 让 `forward-word' 和 `back-backward’ 在中文环境下正常工作
+  (global-set-key (kbd "M-f") 'pyim-forward-word)
+  (global-set-key (kbd "M-b") 'pyim-backward-word)
+
   ;; 开启拼音搜索功能
   (pyim-isearch-mode 1)
 
@@ -34,18 +52,16 @@
   (setq pyim-page-tooltip 'popup)
   (when IS-LINUX (setq x-gtk-use-system-tooltips t))
 
-  ;; 选词框显示5个候选词
-  (setq pyim-page-length 5)
+  ;; 选词框显示7个候选词
+  (setq pyim-page-length 7)
 
   ;; (setq pyim-dicts
   ;;       '((:name "bigdict" :file "/home/vagrant/Downloads/pyim-bigdict.pyim")))
 
-  ;; 让 Emacs 启动时自动加载 pyim 词库
+
+  ;; 让 Emacs 启动时自动加载词库
   (add-hook 'emacs-startup-hook
             #'(lambda () (pyim-restart-1 t)))
-  :bind
-  ;;(("M-j" . pyim-convert-code-at-point) ;与 pyim-probe-dynamic-english 配合
-  ;; ("C-;" . pyim-delete-word-from-personal-buffer)))
   )
 
 (provide 'module-input)
